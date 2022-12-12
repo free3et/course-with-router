48710a387f547f5d406c03dc55426e482b93d7ae
free3et <free3et@gmail.com>
2022-12-12 14:27:00 +0200

    add error page

---
+++ src/responses/errorPage.jsx
@@ -0,0 +1,13 @@
+import { useRouteError } from "react-router-dom";
+
+export const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <h1>Oops!</h1>
+      <p>Sorry, something went wrong</p>
+      <p>{error.statusText}</p>
+      <p>{error.message}</p>
+    </>
+  );
+};
