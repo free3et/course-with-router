9df2ec7abcdd4955689af159dcd180637a586915
free3et <free3et@gmail.com>
2022-12-12 14:25:59 +0200

    add errorElement

--- src/router.jsx
+++ src/router.jsx
@@ -4,11 +4,13 @@ import { CourseWrapperPage } from "./course/CourseWrapperPage";
 import { ROUTES } from "./routes";
 import { Lesson } from "./course/Lesson";
 import { fetchLessonByIdRouted } from "./responses/fetchLessonById";
+import { ErrorPage } from "./responses/errorPage";
 
 export const router = createBrowserRouter([
   {
     path: ROUTES.lessons,
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
